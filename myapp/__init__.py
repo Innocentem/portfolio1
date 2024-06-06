80da37f371f910729f06ccc3ddbb9eaf85da4515
Innocentem <mwbzinno@gmail.com>
2024-06-06 23:53:34 +0300

    (AB) -zip[D[D[D.zip

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,19 +5,16 @@ from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
-migrate = Migrate()  # Initialize Flask-Migrate
+migrate = Migrate() 
 
 def create_app():
-    # Load environment variables from bots.env file
     load_dotenv('bots.env')
 
     app = Flask(__name__)
 
-    # Load configurations from environment variables
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-    # Telegram bot configuration
     app.config['TELEGRAM_BOT_TOKEN'] = os.getenv('TELEGRAM_BOT_TOKEN')
     app.config['TELEGRAM_CHAT_ID'] = os.getenv('TELEGRAM_CHAT_ID')
 
@@ -25,16 +22,14 @@ def create_app():
         raise RuntimeError("SQLALCHEMY_DATABASE_URI must be set")
 
     db.init_app(app)
-    migrate.init_app(app, db)  # Integrate Flask-Migrate with the app and db
+    migrate.init_app(app, db) 
 
-    # Register blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-    # Import models to ensure they are registered
     with app.app_context():
         from .models import User, Item
         db.create_all()
